ef656fd4fb418c4cf4a5b1d50154545fdac79113
yburkul <yburkul123@gmail.com>
2022-05-25 18:01:28 +0530

    Added:Insert Char In Stack

---
+++ StackProblem/StackProblem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StackProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome In Stack Program");
+            int input;
+            do
+            {
+                Console.WriteLine("\n1:Adding the char item in Stack");
+                Console.WriteLine("0:Exit");
+                input = int.Parse(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        StackExample.InsertChar();
+                        break;
+                    case 0:
+                        Console.WriteLine("Exit");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input: ----- Please Enter Correct Input");
+                        break;
+                }
+            }
+            while (input != 0);
+        }
+    }
+}
---
+++ StackProblem/StackProblem/StackExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProblem
+{
+    public class StackExample
+    {
+        public static void InsertChar()
+        {
+            Stack<char> stack = new Stack<char>();
+            stack.Push('A');
+            stack.Push('B');
+            stack.Push('C');
+            stack.Push('D');
+
+            Console.WriteLine("Current stack: ");
+            foreach (char c in stack)
+            {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
